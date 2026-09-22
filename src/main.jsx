753800785b6e95c36f90db8f08ce7776bbf1b7d6
ihebZm <iheb@cfac.com.tn>
2026-09-22 16:16:44 +0100

    analytic vercelé

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { inject } from "@vercel/analytics";
 import "./index.css";
 import "./i18n"; // Import i18n configuration
 import App from "./App.jsx";
 import { HelmetProvider } from "react-helmet-async";
+
+inject();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
